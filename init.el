3188f85d26a61a6ad4f1313061bc08d1974d8b16
DylanRitchings <dylanritchings1998@gmail.com>
2021-09-27 14:00:04 +0100

    Initial commit

---
+++ init.el
@@ -0,0 +1,29 @@
+;; defaultb
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(cua-mode t nil (cua-base))
+ '(custom-enabled-themes (quote (tango-dark)))
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+
+
+;; Move backups
+    (setq backup-directory-alist
+          `(("." . ,(concat user-emacs-directory "backups"))))
+
+;; Disable toolbar (copy, paste etc)
+(tool-bar-mode -1)
