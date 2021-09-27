852dc9cda7b3a9fe6004c174f08f8a7af84f85ed
DylanRitchings <dylanritchings1998@gmail.com>
2021-09-27 17:57:47 +0100

    Improved basic func

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;; defaultb
+;; default
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -10,7 +10,8 @@
    ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
  '(cua-mode t nil (cua-base))
  '(custom-enabled-themes (quote (tango-dark)))
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(package-selected-packages (quote (multiple-cursors use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -21,9 +22,36 @@
 
 
 
+
+;; Package manager
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; Use package install
+(dolist (package '(use-package))
+   (unless (package-installed-p package)
+       (package-install package)))
+
 ;; Move backups
-    (setq backup-directory-alist
-          `(("." . ,(concat user-emacs-directory "backups"))))
+(setq backup-directory-alist
+      `(("." . ,(concat user-emacs-directory "backups"))))
 
 ;; Disable toolbar (copy, paste etc)
 (tool-bar-mode -1)
+
+
+;; Interactive commands (maybe replace with helm)
+(setq ido-everywhere t)
+(setq ido-enable-flex-matching t)
+(ido-mode t)
+
+;; Multiple cursors
+(use-package multiple-cursors
+  :ensure t)
+
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-?") 'mc/mark-all-like-this)
