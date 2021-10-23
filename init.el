6713349abdbf6612c8898b02b2251bd8dfe0a05a
dylanritchings <dylanritchings1998@gmail.com>
2021-10-23 17:42:03 +0100

    working config terraform-lsp yasnippet

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
  '(lsp-terraform-server "/usr/bin/terraform-ls")
  '(package-selected-packages
    (quote
-    (dap-mode lsp-ui yasnippet which-key use-package terraform-mode multiple-cursors magit dumb-jump bash-completion auto-complete))))
+    (yasnippet-snippets dap-mode lsp-ui yasnippet which-key use-package terraform-mode multiple-cursors magit dumb-jump bash-completion auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -57,7 +57,7 @@
 (which-key-mode)
 ;;;;;;;;;;;;;;
 
-;;;;;;;;;;;;;; NAVIGATION
+;;;;;;;;;;;;;;;;;;;;; NAVIGATION
 
 (global-set-key (kbd "M-RET") 'newline)
 
@@ -71,8 +71,9 @@
 
 (global-set-key (kbd "C-<end>") 'kill-buffer-and-window)
 (global-set-key (kbd "C-<home>") 'kill-this-buffer)
+;;;;;;;;;;;;;;;;;;;;;
 
-;;Dumb Jump
+;;;;;;;;;;;;;;;;;;;;;;;Dumb Jump
 ;; (use-package xref
 ;;   :ensure t)
 
@@ -83,20 +84,22 @@
   :ensure t)
 
 ;; (add-to-list 'xref-backend-functions 'dumb-jump-xref-activate t)
+;;;;;;;;;;;;;;;;;;;;;
 
-;;IDO
+;;;;;;;;;;;;;;;;;;;;;IDO
 (setq ido-everywhere t)
 (setq ido-enable-flex-matching t)
 (ido-mode t)
+;;;;;;;;;;;;;;;;;;;;;
 
-
-;; registers
+;;;;;;;;;;;;;;;;;;;;;; registers
 
 (global-set-key [f4] 'jump-to-register)
 (set-register ?w '(file . "/home/dylan/Dev/work/defra/"))
 (set-register ?d '(file . "/home/dylan/Dev/"))
 
-;; terminal
+;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;; terminal
 
 (defun term-other-window ()
   (interactive)
@@ -106,10 +109,9 @@
 
 (global-set-key [f1] 'term-other-window)
 
+;;;;;;;;;;;;;;;;;;;;;
 
 
-
-;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;AUTO FILES
 ;; Move backups
@@ -162,6 +164,90 @@
 ;;;;;;;;;;;;;;
 
 
+
+
+;;;;;;;;;;;;;; LSP
+
+
+(use-package lsp-mode
+  :defer t
+  :commands lsp
+  :custom
+  (lsp-keymap-prefix "C-x l")
+  (lsp-auto-guess-root nil)
+  (lsp-prefer-flymake nil) ; Use flycheck instead of flymake
+  (lsp-enable-file-watchers nil)
+  (lsp-enable-folding nil)
+  (read-process-output-max (* 1024 1024))
+  (lsp-keep-workspace-alive nil)
+  (lsp-eldoc-hook nil)
+  :config
+  (lsp-register-client
+  (make-lsp-client :new-connection (lsp-stdio-connection '("/usr/bin/terraform-ls" "serve"))
+		    :major-modes '(terraform-mode)
+		    :server-id 'terraform-lsp))
+  :bind (:map lsp-mode-map ("C-c C-f" . lsp-format-buffer))
+  :hook ((java-mode python-mode go-mode rust-mode
+          js-mode js2-mode typescript-mode web-mode
+          c-mode c++-mode objc-mode terraform-mode) . lsp-deferred)
+  :config
+  (defun lsp-update-server ()
+    "Update LSP server."
+    (interactive)
+    ;; Equals to `C-u M-x lsp-install-server'
+    (lsp-install-server t)))
+
+;; (use-package lsp-mode
+;;   :commands (lsp lsp-deferred)
+;;   :init (setq lsp-keymap-prefix "C-c l")
+;;   :config
+;;   (lsp-enable-which-key-integration t)
+;;   (lsp-register-client
+;;    (make-lsp-client :new-connection (lsp-stdio-connection '("/usr/bin/terraform-ls" "serve"))
+;;   ;; (make-lsp-client :new-connection (lsp-stdio-connection '("/home/dylan/terraform-lsp" "serve"))
+;; 		    :major-modes '(terraform-mode)
+;; 		    :server-id 'terraform-lsp)))
+;; ;; (setq lsp-terraform-enable-logging t)
+;; ;; (setq lsp-log-io nil)
+;; ;; (setq lsp-modeline-diagnostics-enable t)
+
+(use-package lsp-ui
+  :ensure t
+  ;; :requires flycheck
+  ;; :after lsp-mode
+  )
+;; (setq lsp-ui-imenu-auto-refresh t)
+
+
+;; ;; Dap Mode
+
+;; (use-package dap-mode
+;;   :ensure t)
+
+
+;;;;;;;;;;;;;;; ;;COMPANY MODE
+(use-package company
+  :ensure t)
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; Terraform
+
+(use-package terraform-mode
+  :ensure t
+  ;; :hook (terraform-mode . lsp-deferred)
+  )
+
+(add-hook 'terraform-mode-hook #'terraform-format-on-save-mode)
+
+(use-package hcl-mode
+  :ensure t)
+
+
+
+;;;;;;;;;;;;;;
+
+
+
 ;; ;;;;;;;;;;;;;; FLYCHECK
 (use-package flycheck
   :ensure t
@@ -175,58 +261,12 @@
 ;;   :error-parser flycheck-parse-checkstyle
 ;;   :error-filter flycheck-dequalify-error-ids
 ;;   :modes (terraform-mode))
-
 ;; (add-to-list 'flycheck-checkers 'tflint)
-;;;;;;;;;;;;;;
 
-;;;;;;;;;;;;;; LSP
-
-(use-package lsp-mode
-  :commands (lsp lsp-deferred)
-  :init (setq lsp-keymap-prefix "C-c l")
-  :config
-  (lsp-enable-which-key-integration t)
-  (lsp-register-client
-   (make-lsp-client :new-connection (lsp-stdio-connection '("/usr/bin/terraform-ls" "serve"))
-		    :major-modes '(terraform-mode)
-		    :server-id 'terraform-lsp)))
-;; (setq lsp-terraform-enable-logging t)
-;; (setq lsp-log-io nil) 
-
-(use-package lsp-ui
-  :ensure t)
-
-
-;; Dap Mode
-
-(use-package dap-mode
-  :ensure t)
-
-;; Terraform
-
-(use-package terraform-mode
-  :ensure t
-  :hook (terraform-mode . lsp-deferred)
-  )
-
-(add-hook 'terraform-mode-hook #'terraform-format-on-save-mode)
-
-(use-package hcl-mode
-  :ensure t)
-
-
-
-;; (setq lsp-modeline-diagnostics-enable t)
 
 ;;;;;;;;;;;;;;
 
 
-;; ;;;;;;;;;;;;;; COMPANY MODE
-(use-package company
-  :ensure t)
-(add-hook 'after-init-hook 'global-company-mode)
-
-;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;; KILL WORD BACKSPACE
 
 
@@ -267,3 +307,34 @@
             'aborn/backward-kill-word)
 
 ;;;;;;;;;;;;;;; END KILL WORD BACKSPACE
+
+;;;;;;;;;;;;;;; YASnippet
+
+(use-package yasnippet
+  :ensure t
+  :diminish yas-minor-mode
+  :init
+  (use-package yasnippet-snippets :after yasnippet :ensure t)
+  :hook ((prog-mode LaTeX-mode org-mode) . yas-minor-mode)
+  :bind
+  (:map yas-minor-mode-map ("C-c C-n" . yas-expand-from-trigger-key))
+  (:map yas-keymap
+        (("TAB" . smarter-yas-expand-next-field)
+         ([(tab)] . smarter-yas-expand-next-field)))
+  :config
+  (yas-reload-all)
+  (defun smarter-yas-expand-next-field ()
+    "Try to `yas-expand' then `yas-next-field' at current cursor position."
+    (interactive)
+    (let ((old-point (point))
+          (old-tick (buffer-chars-modified-tick)))
+      (yas-expand)
+      (when (and (eq old-point (point))
+                 (eq old-tick (buffer-chars-modified-tick)))
+        (ignore-errors (yas-next-field))))))
+
+;;;;;;;;;;;;;;; END YASnippet
+
+
+
+
